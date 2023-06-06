239f398974391231cfab856d69a9c0eca92e9df2
JustinLeighton <Justinthomas.leighton@gmail.com>
2023-06-05 22:32:44 -0500

    Cleaned up script

--- BifurcationDiagram.py
+++ BifurcationDiagram.py
@@ -12,112 +12,27 @@ from matplotlib import transforms as mtransforms
 import gc
 import os
 
-def logistic(r, x):
-    return r * x * (1 - x)
-
-
-
-#%% Plot logistic function
-
-x = np.linspace(0, 1)
-fig, ax = plt.subplots(1, 1)
-ax.plot(x, logistic(2, x), 'k')
-
-
-
-#%% Plot example
-
-def plot_system(r, x0, n, ax=None):
-    # Plot the function and the
-    # y=x diagonal line.
-    t = np.linspace(0, 1)
-    ax.plot(t, logistic(r, t), 'k', lw=2)
-    ax.plot([0, 1], [0, 1], 'k', lw=2)
-
-    # Recursively apply y=f(x) and plot two lines:
-    # (x, x) -> (x, y)
-    # (x, y) -> (y, y)
-    x = x0
-    for i in range(n):
-        y = logistic(r, x)
-        # Plot the two lines.
-        ax.plot([x, x], [x, y], 'k', lw=1)
-        ax.plot([x, y], [y, y], 'k', lw=1)
-        # Plot the positions with increasing
-        # opacity.
-        ax.plot([x], [y], 'ok', ms=10,
-                alpha=(i + 1) / n)
-        x = y
-
-    ax.set_xlim(0, 1)
-    ax.set_ylim(0, 1)
-    ax.set_title(f"$r={r:.1f}, \, x_0={x0:.1f}$")
-
-
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6),
-                               sharey=True)
-plot_system(2.5, .1, 10, ax=ax1)
-plot_system(3.5, .1, 10, ax=ax2)
-
-
-
-#%% Dual plot with lyapunov exponent
-
-n = 10000
-r = np.linspace(2.5, 4.0, n)
-
-iterations = 1000
-last = 100
-
-x = 1e-5 * np.ones(n)
-
-lyapunov = np.zeros(n)
-
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 9),
-                               sharex=True)
-for i in range(iterations):
-    x = logistic(r, x)
-    # We compute the partial sum of the
-    # Lyapunov exponent.
-    lyapunov += np.log(abs(r - 2 * r * x))
-    # We display the bifurcation diagram.
-    if i >= (iterations - last):
-        ax1.plot(r, x, ',k', alpha=.25)
-ax1.set_xlim(2.5, 4)
-ax1.set_title("Bifurcation diagram")
-
-# We display the Lyapunov exponent.
-# Horizontal line.
-ax2.axhline(0, color='k', lw=.5, alpha=.5)
-# Negative Lyapunov exponent.
-ax2.plot(r[lyapunov < 0],
-         lyapunov[lyapunov < 0] / iterations,
-         '.k', alpha=.5, ms=.5)
-# Positive Lyapunov exponent.
-ax2.plot(r[lyapunov >= 0],
-         lyapunov[lyapunov >= 0] / iterations,
-         '.r', alpha=.5, ms=.5)
-ax2.set_xlim(2.5, 4)
-ax2.set_ylim(-2, 1)
-ax2.set_title("Lyapunov exponent")
-plt.tight_layout()
-
-
 
 #%% Plot large bifurcation diagram
 
+# Function parameters
 n = 100000
 r = np.linspace(2.5, 4.0, n)
 iterations = 1000
 last = 100
 x = 1e-5 * np.ones(n)
 
+# Define logistic map function
+def logistic(r, x):
+    return r * x * (1 - x)
+
+# Initialize custom scaling class
 class CustomScale(mscale.ScaleBase):
     name = 'custom'
 
     def __init__(self, axis, **kwargs):
         mscale.ScaleBase.__init__(self)
-        self.thresh = None #thresh
+        self.thresh = None
 
     def get_transform(self):
         return self.InvertedCustomTransform(self.thresh)
@@ -133,25 +48,26 @@ class CustomScale(mscale.ScaleBase):
         def transform_non_affine(self, a):
             return np.exp(a)-1
 
+# Initialize plot surface
 mscale.register_scale(CustomScale)
 fig, ax = plt.subplots(1, 1, figsize=(177,126))
 ax.set_facecolor((49/255, 52/255, 58/255))
 
+# Plot points
 for i in range(iterations):
     x = logistic(r, x)
     if i >= (iterations - last):
         ax.plot(r, x, ',k', alpha=.25, color="white", marker='.', markersize=2)
+
+# Customize plot for clean image
 ax.set_xlim(2.5, 4)
-
 ax.set_xscale('custom')
-
 ax.xaxis.set_visible(False)
 ax.yaxis.set_visible(False)
-
 [spine.set_visible(False) for spine in ax.spines.values()]
-
 #r.set_clip_on(False)
 
+# Save image
 plt.savefig('bifurcation_diagram_e2.png', bbox_inches='tight')
 
 # Cleanup
